f0ea64082cfb0c7c07577492ee37461839ff7b1c
roman1pr <iroman.pri@gmail.com>
2015-05-06 22:49:24 +0300

    Initial Commit

---
+++ bitwise/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  bitwise
+//
+//  Created by Роман Приискалов on 06/05/15.
+//  Copyright (c) 2015 Роман Приискалов. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ bitwise/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  bitwise
+//
+//  Created by Роман Приискалов on 06/05/15.
+//  Copyright (c) 2015 Роман Приискалов. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
